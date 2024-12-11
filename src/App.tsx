5dcd76a025444ee18f8d851ecee8dece52626b68
gabkun <95224438+gabkun@users.noreply.github.com>
2024-12-11 21:54:43 +0800

    with build

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,13 @@
-import { useState } from 'react'
 import Navbar from './Components/Navbar'
 import Login from './Components/Login'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SignupPage from './Components/Signup'
-import PrivateRoute from './routes/PrivateRoute';
 import LandingPage from './Components/Landingpage';
 
 
 
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
 <>
